a6365af1656d2a91a50d321a06f1275a02871b31
MinuteLinuxBen <smitb193@mail.broward.edu>
2022-11-12 19:02:10 -0500

    Lab 5 Files

---
+++ js/funscripts.js
@@ -0,0 +1,3 @@
+var yourName = prompt('Please enter your name: ');
+var yourMajor = prompt('What are you currently majoring in?');
+document.write("Great, we love " + yourMajor + " majors in this club!");
